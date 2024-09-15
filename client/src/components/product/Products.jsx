d23ddf41e1b805d0a5442d9a924881a2acd42bab
kmtusher97 <kmtusher97@gmail.com>
2024-09-15 22:30:01 +0600

    Fetch products using useEffect hook

--- client/src/components/product/Products.jsx
+++ client/src/components/product/Products.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { useProducts } from '../../api/queries';
 
 import { ProductCard } from './ProductCard';
@@ -5,7 +7,14 @@ import { ProductCard } from './ProductCard';
 import './Products.css';
 
 export function Products() {
-  const { products } = useProducts();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/api/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  // const { products } = useProducts();
 
   return (
     <div className="product-container">
