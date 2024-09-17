5b2f4c1725a100a923ab7f599c0ce5aaf3f205f3
kmtusher97 <kmtusher97@gmail.com>
2024-09-17 22:00:13 +0600

    Add axios wrapper
    Add error handler in useProducts hook

--- client/src/api/services/productServices.js
+++ client/src/api/services/productServices.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import { http } from '../../common/http';
 
 export const getProducts = async () => {
-  const response = await axios.get('http://localhost:5000/api/products');
+  const response = await http.get('/api/products');
   return response.data;
 };
