4e4e1f6d049d12295dff9f549231d0b067fe0092
kmtusher97 <kmtusher97@gmail.com>
2024-09-20 21:22:49 +0600

    Add loading state while fetching data

--- client/src/api/queries/useProducts.jsx
+++ client/src/api/queries/useProducts.jsx
@@ -3,15 +3,19 @@ import { useEffect, useState } from 'react';
 import { ProductServices } from '../services';
 
 export const useProducts = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
 
   const loadProducts = async () => {
     try {
+      setIsLoading(true);
       const products = await ProductServices.getProducts();
       setProducts(products);
     } catch (error) {
       alert('Failed to load products!');
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -19,5 +23,5 @@ export const useProducts = () => {
     loadProducts();
   }, []);
 
-  return { products };
+  return { isLoading, products };
 };
--- client/src/components/product/Products.jsx
+++ client/src/components/product/Products.jsx
@@ -1,17 +1,23 @@
 import { useProducts } from '../../api/queries';
 
+import { Loading } from '../../common/components';
+
 import { ProductCard } from './ProductCard';
 
 import './Products.css';
 
 export function Products() {
-  const { products } = useProducts();
+  const { isLoading, products } = useProducts();
 
   return (
     <div className="product-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      )}
     </div>
   );
 }
