6d0ef8621bdeddf72b74585efc3f9cb84c49f058
kmtusher97 <kmtusher97@gmail.com>
2024-12-21 21:24:40 +0600

    Add local env file

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
