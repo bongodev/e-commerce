6a4ce6ac84f94f55f9435c905f5ed4fdd643696b
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-01-07 11:44:44 +0600

    create products from the client to the server and fetch the all products from the server to the client

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,1 +1,108 @@
-import express from 'express'
+import express from 'express';
+import { Product } from '../models/index.js';
+import { authenticateToken } from '../middlewares/index.js';
+
+const router = express.Router();
+
+//create product
+router.post('/',authenticateToken, async(req,res) =>{
+    try{
+        const userId=req.user._id;
+        const productObj={
+            name:req.body.name,
+            price: parseInt(req.body.price),
+            userId: userId,
+            quantity: parseInt(req.body.quantity),
+            isDeleted: false,
+        };
+        const product= new Product(productObj)
+        await product.save()
+        return res.status(201).json(product)
+    }catch(error){
+        res.status(500).json({message:"Something went wrong"})
+    }
+})
+
+//get all products
+router.get("/", authenticateToken, async (req, res) => {
+    try {
+        let current = req?.query?.current ?? "1";
+        current = parseInt(current);
+
+        let pageSize = req?.query?.pageSize ?? "10"; // Default page size to 10
+        pageSize = parseInt(pageSize);
+
+        const sort = req?.query?.sort ?? "asc";
+        const pipeline = [];
+
+        pipeline.push({
+            $match: {
+                isDeleted: false,
+            },
+        });
+
+        switch (sort) {
+            case "asc":
+                pipeline.push({
+                    $sort: {
+                        createdAt: 1,
+                    },
+                });
+                break;
+
+            case "desc":
+                pipeline.push({
+                    $sort: {
+                        createdAt: -1,
+                    },
+                });
+                break;
+        }
+
+        pipeline.push({
+            $skip: (current - 1) * pageSize,
+        });
+
+        pipeline.push({
+            $limit: pageSize,
+        });
+
+        pipeline.push({
+            $lookup: {
+                from: "users",
+                localField: "userId",
+                foreignField: "_id",
+                as: "user",
+            },
+        });
+
+        pipeline.push({
+            $lookup: {
+                from: "files",
+                localField: "fileId",
+                foreignField: "_id",
+                as: "file",
+            },
+        });
+
+        let products = await Product.aggregate(pipeline);
+
+        // Add `id` field to each product
+        products = products.map((product) => ({
+            id: product._id, // Map MongoDB `_id` to `id`
+            ...product,
+        }));
+
+        return res.json(products);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+});
+
+  
+
+
+
+
+export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { appConfig } from './config/index.js';
 import connectDB from './db.js';
 import configureRoutes from './routes/index.js';
-import { authenticateToken } from './middlewares/index.js';
+//import { authenticateToken } from './middlewares/index.js';
 
 const app = express();
 
@@ -21,169 +21,169 @@ app.use(
 configureRoutes(app);
 
 // In-memory array to store products
-let products = [
-  {
-    id: 1,
-    name: 'Gaming Laptop',
-    quantity: 10,
-    price: 1500,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Laptops', 'Gaming'],
-  },
-  {
-    id: 2,
-    name: 'Wireless Mouse',
-    quantity: 10,
-    price: 50,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Accessories', 'Peripherals'],
-  },
-  {
-    id: 3,
-    name: 'Mechanical Keyboard',
-    quantity: 10,
-    price: 100,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Accessories', 'Peripherals'],
-  },
-  {
-    id: 4,
-    name: 'External Hard Drive',
-    quantity: 10,
-    price: 120,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Storage', 'Accessories'],
-  },
-  {
-    id: 5,
-    name: 'Graphics Card',
-    quantity: 10,
-    price: 500,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Components', 'Gaming'],
-  },
-  {
-    id: 6,
-    name: 'Portable SSD',
-    quantity: 10,
-    price: 200,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Storage', 'Accessories'],
-  },
-  {
-    id: 7,
-    name: 'Gaming Monitor',
-    quantity: 10,
-    price: 300,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Monitors', 'Gaming'],
-  },
-  {
-    id: 8,
-    name: 'All-in-One Printer',
-    quantity: 10,
-    price: 150,
-    image: 'https://via.placeholder.com/150',
-    categories: ['Peripherals', 'Printers'],
-  },
-];
+// let products = [
+//   {
+//     id: 1,
+//     name: 'Gaming Laptop',
+//     quantity: 10,
+//     price: 1500,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Laptops', 'Gaming'],
+//   },
+//   {
+//     id: 2,
+//     name: 'Wireless Mouse',
+//     quantity: 10,
+//     price: 50,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Accessories', 'Peripherals'],
+//   },
+//   {
+//     id: 3,
+//     name: 'Mechanical Keyboard',
+//     quantity: 10,
+//     price: 100,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Accessories', 'Peripherals'],
+//   },
+//   {
+//     id: 4,
+//     name: 'External Hard Drive',
+//     quantity: 10,
+//     price: 120,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Storage', 'Accessories'],
+//   },
+//   {
+//     id: 5,
+//     name: 'Graphics Card',
+//     quantity: 10,
+//     price: 500,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Components', 'Gaming'],
+//   },
+//   {
+//     id: 6,
+//     name: 'Portable SSD',
+//     quantity: 10,
+//     price: 200,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Storage', 'Accessories'],
+//   },
+//   {
+//     id: 7,
+//     name: 'Gaming Monitor',
+//     quantity: 10,
+//     price: 300,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Monitors', 'Gaming'],
+//   },
+//   {
+//     id: 8,
+//     name: 'All-in-One Printer',
+//     quantity: 10,
+//     price: 150,
+//     image: 'https://via.placeholder.com/150',
+//     categories: ['Peripherals', 'Printers'],
+//   },
+// ];
 
 // Get all products
-app.get('/api/products', (req, res) => {
-  const { category } = req.query;
-  let filters = [];
-  if (typeof category === 'string') {
-    filters = [category.toLowerCase()];
-  } else if (Array.isArray(category) && category.length) {
-    filters = category.map((category) => category.toLowerCase());
-  }
-  if (!filters.length) {
-    res.json(products.sort((a, b) => b.id - a.id));
-  }
-  res.json(
-    products
-      .filter((product) =>
-        product.categories.some((category) =>
-          filters.includes(category.toLowerCase())
-        )
-      )
-      .sort((a, b) => b.id - a.id)
-  );
-});
+// app.get('/api/products', (req, res) => {
+//   const { category } = req.query;
+//   let filters = [];
+//   if (typeof category === 'string') {
+//     filters = [category.toLowerCase()];
+//   } else if (Array.isArray(category) && category.length) {
+//     filters = category.map((category) => category.toLowerCase());
+//   }
+//   if (!filters.length) {
+//     res.json(products.sort((a, b) => b.id - a.id));
+//   }
+//   res.json(
+//     products
+//       .filter((product) =>
+//         product.categories.some((category) =>
+//           filters.includes(category.toLowerCase())
+//         )
+//       )
+//       .sort((a, b) => b.id - a.id)
+//   );
+// });
 
 // Get a product by ID
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p.id === parseInt(req.params.id));
-  if (!product) return res.status(404).json({ message: 'Product not found' });
-  res.json(product);
-});
+// app.get('/api/products/:id', (req, res) => {
+//   const product = products.find((p) => p.id === parseInt(req.params.id));
+//   if (!product) return res.status(404).json({ message: 'Product not found' });
+//   res.json(product);
+// });
 
 // Create a new product
-app.post('/api/products', (req, res) => {
-  const newProduct = {
-    id: products.reduce((max, cur) => Math.max(max, cur.id), 0) + 1,
-    name: req.body.name,
-    quantity: req.body.quantity,
-    price: req.body.price,
-    vendor: req.body.vendor,
-    categories: req.body.categories,
-  };
+// app.post('/api/products', (req, res) => {
+//   const newProduct = {
+//     id: products.reduce((max, cur) => Math.max(max, cur.id), 0) + 1,
+//     name: req.body.name,
+//     quantity: req.body.quantity,
+//     price: req.body.price,
+//     vendor: req.body.vendor,
+//     categories: req.body.categories,
+//   };
 
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
+//   products.push(newProduct);
+//   res.status(201).json(newProduct);
+// });
 
 // Update an existing product
-app.put('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p.id === parseInt(req.params.id));
-  if (!product) return res.status(404).json({ message: 'Product not found' });
+// app.put('/api/products/:id', (req, res) => {
+//   const product = products.find((p) => p.id === parseInt(req.params.id));
+//   if (!product) return res.status(404).json({ message: 'Product not found' });
 
-  product.name = req.body.name || product.name;
-  product.quantity = req.body.quantity || product.quantity;
-  product.price = req.body.price || product.price;
-  product.vendor = req.body.vendor || product.vendor;
-  product.categories = req.body.categories || product.categories;
+//   product.name = req.body.name || product.name;
+//   product.quantity = req.body.quantity || product.quantity;
+//   product.price = req.body.price || product.price;
+//   product.vendor = req.body.vendor || product.vendor;
+//   product.categories = req.body.categories || product.categories;
 
-  res.json(product);
-});
+//   res.json(product);
+// });
 
 // Delete a product
-app.delete('/api/products/:id', authenticateToken, (req, res) => {
-  const productIndex = products.findIndex(
-    (p) => p.id === parseInt(req.params.id)
-  );
-  if (productIndex === -1)
-    return res.status(404).json({ message: 'Product not found' });
+// app.delete('/api/products/:id', authenticateToken, (req, res) => {
+//   const productIndex = products.findIndex(
+//     (p) => p.id === parseInt(req.params.id)
+//   );
+//   if (productIndex === -1)
+//     return res.status(404).json({ message: 'Product not found' });
 
-  const deletedProduct = products.splice(productIndex, 1);
-  res.json(deletedProduct);
-});
+//   const deletedProduct = products.splice(productIndex, 1);
+//   res.json(deletedProduct);
+// });
 
 // Search for products
-app.get('/api/search', (req, res) => {
-  const { name, vendor, category } = req.query;
-  let filteredProducts = products;
+// app.get('/api/search', (req, res) => {
+//   const { name, vendor, category } = req.query;
+//   let filteredProducts = products;
 
-  if (name) {
-    filteredProducts = filteredProducts.filter((product) =>
-      product.name.toLowerCase().includes(name.toLowerCase())
-    );
-  }
+//   if (name) {
+//     filteredProducts = filteredProducts.filter((product) =>
+//       product.name.toLowerCase().includes(name.toLowerCase())
+//     );
+//   }
 
-  if (vendor) {
-    filteredProducts = filteredProducts.filter((product) =>
-      product.vendor.toLowerCase().includes(vendor.toLowerCase())
-    );
-  }
+//   if (vendor) {
+//     filteredProducts = filteredProducts.filter((product) =>
+//       product.vendor.toLowerCase().includes(vendor.toLowerCase())
+//     );
+//   }
 
-  if (category) {
-    filteredProducts = filteredProducts.filter((product) =>
-      product.category.toLowerCase().includes(category.toLowerCase())
-    );
-  }
+//   if (category) {
+//     filteredProducts = filteredProducts.filter((product) =>
+//       product.category.toLowerCase().includes(category.toLowerCase())
+//     );
+//   }
 
-  res.json(filteredProducts);
-});
+//   res.json(filteredProducts);
+// });
 
 // Start the server
 app.listen(appConfig.PORT, () => {
