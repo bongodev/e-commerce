537158449fead452d1d5ff189c4ab4703cc81a52
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-01-09 12:39:01 +0600

    Replace products mock description with proper implementation

--- server/src/models/product.js
+++ server/src/models/product.js
@@ -6,20 +6,17 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    // description:{
-    //     type:String
-    // },
-    // madeIn:{
-    //     type:String
-    // },
-    // category:{
-    //     type:String
-    // },
+    description:{
+        type:String,
+        required: true,
+    },
     price: {
       type: Number,
+      required: true,
     },
     quantity: {
       type: Number,
+      required: true,
     },
     isDeleted: {
       type: Boolean,
